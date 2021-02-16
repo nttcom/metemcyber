e97af2a8073f62d8b9eae2ef05103c217cff798f
Takuya Nishino <takuya.nishino@ntt.com>
2021-02-16 18:06:21 +0900

    optimize command line

--- src/metemctl_config.py
+++ src/metemctl_config.py
@@ -15,10 +15,11 @@
 #
 
 """
-usage: metemctl config <command> [<target>] [<value>]
+usage:
+    metemctl config [options] [<key>] [<value>]
 
-options:
-   -h, --help
+    -h, --help
+    -l, --list      Print option values within the general section
 
 """
 from docopt import docopt
@@ -29,35 +30,24 @@ CONFIG_INI_FILEPATH = "metemctl.ini"
 
 if __name__ == '__main__':
 
-    args = docopt(__doc__,
-                  options_first=True)
-    
+    args = docopt(__doc__, help=True)
+        
     config = configparser.ConfigParser()
     config.add_section('general')
     config.read(CONFIG_INI_FILEPATH)
-    
-    do = args['<command>']
-    target = args['<target>']
-    value = args['<value>']
 
-    # get command
-    if do == "get":
-        if target:
-            print(config['general'][target])
-        else:
-            print("Please specify the <target> in the 'get' command. See 'metemctl config --help'.")
-    # set command
-    elif do == "set" and target:
-        if target and value:
-            config.set('general', target, value)
-            with open(CONFIG_INI_FILEPATH, 'w') as fout:
-                config.write(fout)
-                print('update config.')
-        else:
-            print("Please specify the <target> and <value> in the 'set' command. See 'metemctl config --help'.")
-    # list command
-    elif do == "list":
+    # get key
+    if args['<key>']:
+        print(config['general'][args['<key>']])
+    # set key=value
+    elif args['<key>'] and args['<value>']:
+        config.set('general', args['<key>'], args['<value>'])
+        with open(CONFIG_INI_FILEPATH, 'w') as fout:
+            config.write(fout)
+            print('update config.')
+    # print all option values
+    elif args['--list']:
         for option in config['general']:
             print(option, ":", config['general'][option])
     else:
-        exit("%r is not valid in the config command. See 'metemctl config --help'." % args['<command>'])
+        exit("%r is not valid in the config command. See 'metemctl config --help'.")
