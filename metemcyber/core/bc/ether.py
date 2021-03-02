2163f79cd8ac4d066e6b1f85c2245c167796682d
Takuya Nishino <takuya.nishino@ntt.com>
2021-03-02 12:30:53 +0900

    embed the signature in web3

--- metemcyber/core/bc/ether.py
+++ metemcyber/core/bc/ether.py
@@ -14,10 +14,42 @@
 #    limitations under the License.
 #
 
-from web3 import Web3
+import web3
+from web3 import Account, Web3
+from web3.exceptions import ExtraDataLengthError
+from web3.middleware import (construct_sign_and_send_raw_middleware,
+                             geth_poa_middleware)
 from web3.providers.rpc import HTTPProvider
 
 
 class Ether:
     def __init__(self, endpoint):
         self.web3 = Web3(HTTPProvider(endpoint))
+        if self.web3.isConnected():
+            try:
+                self.web3.eth.getBlock('latest')
+            except ExtraDataLengthError:
+                self.web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+    def signature(self, private_key):
+
+        # 署名付きトランザクションの準備とcoinbaseアカウントの作成
+        cssrm = construct_sign_and_send_raw_middleware(private_key)
+        account_id = Account.from_key(private_key)
+
+        # プライベート鍵を明示的にメモリから削除
+        del private_key
+
+        # ミドルウェアに署名付きトランザクションを設定
+        # See https://github.com/ethereum/web3.py/blob/master/web3/datastructures.py
+        if 'sign_and_send_raw' in self.web3.middleware_onion._queue:
+            self.web3.middleware_onion.replace('sign_and_send_raw', cssrm)
+        else:
+            self.web3.middleware_onion.add(cssrm, 'sign_and_send_raw')
+
+        # coinbaseとしてアカウントを設定
+        self.web3.eth.defaultAccount = account_id
+
+    def web3_with_signature(self, private_key):
+        self.signature(private_key)
+        return self.web3
