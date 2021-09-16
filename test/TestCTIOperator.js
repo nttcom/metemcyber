f17946005ef77ccc94d1f50282b1b6a6d978c651
Kazuaki Anami <kazuaki@soum.co.jp>
2021-09-16 15:25:12 +0900

    refine test code for CTIOperator

--- test/TestCTIOperator.js
+++ test/TestCTIOperator.js
@@ -17,33 +17,134 @@
 const CTIOperator = artifacts.require("CTIOperator");
 const CTIToken = artifacts.require("CTIToken");
 const {format} = require("util");
+const { BN, constants, expectEvent, expectRevert, singletons } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
 
 contract("CTIOperator", async accounts => {
-  it("register and accepted test", async () => {
-    let operator = await CTIOperator.deployed();
-    let token = await CTIToken.deployed();
+  const owner = accounts[0];  // token owner
+  const solver = accounts[1];  // solver (should be authorized by token owner)
+  const anon = accounts[2];  // operator deployer
+  const user = accounts[3];  // token user
+  var operator;
+  var token;
+  var taskId;
+  const nilHex = web3.utils.asciiToHex("");
+  const expectedNilTokens = [].toString();
+  var expectedRegisteredTokens;
 
-    // set ERC777 recipient
+  it("deploy and recipientFor", async () => {
+    operator = await CTIOperator.new({from: anon});
     await operator.recipientFor(operator.address);
-
-    //await operator.register(token, {from: accounts[1]});
-    await operator.register([token.address], {from: accounts[1]});
-
-    // Send Token to operator
-    await token.send(operator.address, 1, web3.utils.asciiToHex(""));
-
-    // Get Event
-    let events = await operator.getPastEvents('TokensReceivedCalled', {fromBlock: 0, toBlock: 'latest'});
-    //console.log(format("Received Events: %o", events));
-
-    let taskId = events[0].returnValues.taskId;
-    //console.log(format("taskId: %d", taskId));
-
-    let tx_receipt = await operator.accepted(taskId, {from: accounts[1]});
-    //console.log(format("tx_receipt: %o", tx_receipt));
-    assert.equal(tx_receipt.receipt.status, true);
-
-    let registereds = await operator.listRegistered(accounts[1]);
-    assert.equal(registereds.sort().toString() == [token.address].sort().toString(), true);
   });
+
+  it("register by token owner", async () => {
+    token = await CTIToken.new(1, [], false, {from: owner});
+    await operator.register([token.address], {from: owner});
+    let results = await operator.checkRegistered([token.address], {from: owner});
+    assert.equal(results[0], true, "checkRegistered mismatch");
+    expectedRegisteredTokens = [token.address].sort().toString();
+  });
+
+  it("register by solver", async () => {
+    await expectRevert(operator.register([token.address], {from: solver}), "not authorized");
+    let results = await operator.checkRegistered([token.address], {from: solver});
+    assert.equal(results[0], false, "checkRegistered mismatch");
+  });
+
+  it("register by authorized solver", async () => {
+    await token.authorizeOperator(solver, {from: owner});
+    await operator.register([token.address], {from: solver});
+    await token.revokeOperator(solver, {from: owner});
+    let results = await operator.checkRegistered([token.address], {from: solver});
+    assert.equal(results[0], true, "checkRegistered mismatch");
+  });
+
+  it("register not a token", async () => {
+    await expectRevert(operator.register([operator.address], {from: owner}), "not a token");
+  });
+
+  it("listRegistered", async () => {
+    let results = await operator.listRegistered(solver, {from: solver});
+    assert.equal(results.sort().toString(), expectedRegisteredTokens, "registered mismatch");
+    results = await operator.listRegistered(solver, {from: user});
+    assert.equal(results.sort().toString(), expectedRegisteredTokens, "registered by user mismatch");
+    results = await operator.listRegistered(user, {from: solver});
+    assert.equal(results.sort().toString(), expectedNilTokens, "registered for user mismatch");
+  });
+
+  it("unregister by token owner", async () => {
+    await operator.unregister([token.address], {from: owner});
+    let results = await operator.checkRegistered([token.address], {from: owner});
+    assert.equal(results[0], false, "checkRegistered mismatch");
+  });
+
+  it("unregister by solver", async () => {
+    await operator.unregister([token.address], {from: solver});  // not authorized
+    let results = await operator.checkRegistered([token.address], {from: solver});
+    assert.equal(results[0], false, "checkRegistered mismatch");
+  });
+
+  it("get TokenReceivedCalled event", async () => {
+    await token.send(user, 1, nilHex, {from: owner});
+    assert.equal(await token.balanceOf(user), 1, "token balance mismatch");
+    await token.send(operator.address, 1, nilHex, {from: user});
+    assert.equal(await token.balanceOf(user), 0, "token balance mismatch");
+    assert.equal(await token.balanceOf(operator.address), 1, "token balance mismatch");
+    let events = await operator.getPastEvents('TokensReceivedCalled',
+        {fromBlock: 0, toBlock: 'latest', from: solver});
+    let task = events[0].returnValues;
+    // console.log(task);
+    assert.equal(task.taskId, 0, "task.taskId mismatch");
+    assert.equal(task.token, token.address, "task.token mismatch");
+    assert.equal(task.from, user, "task.seeker mismatch");
+    taskId = task.taskId;
+  });
+
+  it("accept without register by solver", async () => {
+    await expectRevert(operator.accepted(taskId, {from: solver}), "Not registered.");
+  });
+
+  it("register and accept by solver", async () => {
+    await token.authorizeOperator(solver, {from: owner});
+    await operator.register([token.address], {from: solver});
+    await token.revokeOperator(solver, {from: owner});
+    await expectRevert(operator.accepted(taskId, {from: solver}), "not authorized");
+  });
+
+  it("register and accept by authorized solver", async () => {
+    await token.authorizeOperator(solver, {from: owner});
+    await operator.register([token.address], {from: solver});
+    await operator.accepted(taskId, {from: solver});
+    let task = await operator.latest({from: solver});
+    // console.log(task);
+    assert.equal(task.taskId, taskId, "task.taskId mismatch");
+    assert.equal(task.token, token.address, "task.token mismatch");
+    assert.equal(task.solver, solver, "task.solver mismatch");
+    assert.equal(task.seeker, user, "task.seeker mismatch");
+    assert.equal(task.state, 1, "task.state mismatch");  // TaskState.Accepted == 1
+  });
+
+  it("accept twice", async () => {
+    await expectRevert(operator.accepted(taskId, {from: solver}), "Already accepted.");
+  });
+
+  it("finish by not solver", async () => {
+    await expectRevert(operator.finish(taskId, nilHex, {from: owner}), "not task's solver");
+  });
+
+  it("finish by revoked solver", async () => {  // allowed because already accepted
+    await token.revokeOperator(solver, {from: owner});
+    await operator.finish(taskId, nilHex, {from: solver});
+    let task = await operator.latest({from: solver});
+    // console.log(task);
+    assert.equal(task.taskId, taskId, "task.taskId mismatch");
+    assert.equal(task.state, 2, "task.state mismatch");  // TaskState.Finished == 2
+  });
+
+  it("sendback finished token", async () => {
+    assert.equal(await token.balanceOf(operator.address), 0, "token balance mismatch");
+    assert.equal(await token.balanceOf(user), 1, "token balance mismatch");
+  });
+
 });
