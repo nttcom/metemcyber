f68e7956cb097859ca81c0b47f41992db3460c86
Shunta Ohtsubo <s.ohtsubo@UG057-OHTB-MBP-2106.local>
2021-09-16 10:40:18 +0900

    Add contribution graph

--- gui/src/containers/account.js
+++ gui/src/containers/account.js
@@ -14,18 +14,186 @@
  *    limitations under the License.
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Card, CardHeader, CardBody, CardTitle, Col, Container, List, ListInlineItem, Row, Table } from 'reactstrap';
+import { Button, Card, CardHeader, CardBody, CardTitle, Col, Container, Input, InputGroup, List, ListInlineItem, Spinner, Row, Table, Tooltip } from 'reactstrap';
+import HeatMap from 'react-heatmap-grid';
 
 
 function Account(props) {
+    const { ipcRenderer } = window
+    const [data, setData] = useState([]);
+    const [dates, setDates] = useState([]);
+    const [totalContribution, setTotalContribution] = useState(0);
+    const [xLabels, setXLabels] = useState([]);
+    const [targetEoa, setTargetEoa] = useState(props.content.summary.eoa_address);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const yLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const monthList = { 0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec' }
+
     useEffect(() => {
+        //get transaction data
+        const currentDate = new Date();
+        const utcEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 1);
+        const utcStartDate = new Date(utcEndDate.getFullYear() - 1, utcEndDate.getMonth(), utcEndDate.getDate() + 1);
+
+        ipcRenderer.send('get-transaction',
+            {
+                address: [props.content.summary.eoa_address],
+                startYear: utcStartDate.getFullYear(),
+                startMonth: utcStartDate.getMonth() + 1,
+                startDate: utcStartDate.getDate(),
+                endYear: utcEndDate.getFullYear(),
+                endMonth: utcEndDate.getMonth() + 1,
+                endDate: utcEndDate.getDate(),
+            });
+
+        ipcRenderer.once('send-transaction', (event, arg) => {
+            console.log(arg)
+            setTransaction(arg.activities);
+            setIsLoading(false);
+        });
+
         return () => console.log('unmounting...');
     }, [])
+
+
+    const handleChange = (e) => {
+        setTargetEoa(e.target.value);
+    }
+
+    const getTransaction = () => {
+        setIsLoading(true);
+        const currentDate = new Date();
+        const utcEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 1);
+        const utcStartDate = new Date(utcEndDate.getFullYear() - 1, utcEndDate.getMonth(), utcEndDate.getDate() + 1);
+
+        let eoaList = [];
+        if (targetEoa !== '') {
+            eoaList = targetEoa.replace(' ', '').split(',');
+        }
+        ipcRenderer.send('get-transaction',
+            {
+                address: eoaList,
+                startYear: utcStartDate.getFullYear(),
+                startMonth: utcStartDate.getMonth() + 1,
+                startDate: utcStartDate.getDate(),
+                endYear: utcEndDate.getFullYear(),
+                endMonth: utcEndDate.getMonth() + 1,
+                endDate: utcEndDate.getDate(),
+            });
+        ipcRenderer.once('send-transaction', (event, arg) => {
+            console.log(arg)
+            setTransaction(arg.activities);
+            setIsLoading(false);
+        });
+
+    }
+
+    const setTransaction = (activities) => {
+        const data = new Array(yLabels.length)
+            .fill(0)
+            .map(() => new Array());
+        const resultDate = new Array(yLabels.length)
+            .fill(0)
+            .map(() => new Array());
+
+        const xlb = [];
+
+        let offsetCount = 0;
+        let countContribution = 0;
+
+        const currentDate = new Date();
+        const endDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+        const startDate = new Date(endDate.getFullYear() - 1, endDate.getMonth(), endDate.getDate() + 1);
+
+        const yearDates = (endDate - startDate) / 86400000 + 1; // 86400000 is Milliseconds for one day
+
+        // Set the '-1' in the cell of last year's part
+        for (let i = 0; i < startDate.getDay(); i++) {
+            data[i].push(-1);
+            resultDate[i].push(new Date())
+        }
+
+        let targetDate = startDate;
+        let monthNum = targetDate.getMonth();
+        let month = monthList[monthNum];
+        for (let i = 0; i < yearDates; i++) {
+            const targetMonth = targetDate.getMonth();
+            if (monthNum !== targetMonth) {
+                monthNum = targetMonth;
+                month = monthList[monthNum];
+            }
+
+            const day = targetDate.getDay();
+
+            let count = 0;
+            for (let val of activities) {
+                if (val["days-offset"] === offsetCount) {
+                    count = val.count;
+                    countContribution += count;
+                    break;
+                }
+            }
+
+            data[day].push(count);
+            resultDate[day].push(`${targetDate.getFullYear()}/${targetDate.getMonth() + 1}/${targetDate.getDate()}`);
+
+            if (day === 0 || offsetCount === 0) {
+                xlb.push(month);
+                month = '';
+            }
+
+            offsetCount++;
+            targetDate = new Date(targetDate.getFullYear(), targetDate.getMonth(), targetDate.getDate() + 1);
+        }
+
+        setTotalContribution(countContribution);
+
+        setXLabels(xlb);
+        setData(data);
+        setDates(resultDate);
+    }
+
     return (
         <div>
             <MainContent>
+                <Card>
+                    <CardHeader>Contribution</CardHeader>
+                    <CardBody>
+                        <AccountCardTitle>{totalContribution} contributions in {new Date().getFullYear()}</AccountCardTitle>
+                        {isLoading ?
+                            <Spinner color="primary" />
+                            :
+                            <HeatMap
+                                xLabels={xLabels}
+                                yLabels={yLabels}
+                                data={data}
+                                height={20}
+                                cellStyle={(background, value, min, max, data, x, y) => (
+                                    {
+                                        background: `rgba(66, 86, 244, ${1 - (max - value) / (max - min)})`,
+                                        border: `solid ${value !== undefined && value > -1 ? "1px" : "0px"} rgba(192, 192, 192,0.25)`,
+                                        borderRadius: "3px",
+                                        fontSize: "11px",
+                                    })}
+                                title={(value, unit, x, y) => {
+                                    if (value > -1) {
+                                        return `${dates[y][x]} ${value} contributions`
+                                    }
+                                }}
+                            />
+                        }
+                        <AccountCardTitle>Displayed EOA address</AccountCardTitle>
+                        <AccountList type="inline">
+                            <InputGroup>
+                                <Input value={targetEoa} onChange={handleChange} placeholder="Please enter the EOA address..." />
+                            </InputGroup>
+                            <Button color="primary" onClick={getTransaction}>Change EOA address</Button>
+                        </AccountList>
+                    </CardBody>
+                </Card>
                 <Card>
                     <CardHeader>Account Information</CardHeader>
                     <CardBody>
