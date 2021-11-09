fa13ab67f1beafdb85cac65d20da375a2f8a60d3
Shunta Ohtsubo <s.ohtsubo@UG057-OHTB-MBP-2106.local>
2021-11-09 12:37:36 +0900

    Fixed the process when the application is closed

--- gui/public/electron.js
+++ gui/public/electron.js
@@ -79,13 +79,6 @@ app.whenReady().then(() => {
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
 
 ipcMain.on('logout', async (event, arg) => {
   process.env.METEMCTL_KEYFILE_PASSWORD = '';
